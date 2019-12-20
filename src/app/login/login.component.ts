3a14a086fac2a139d25b9d1bb3eba52f7c12cd5a
Akaljot Gujjar <akaljotgujjar@gmail.com>
2019-12-19 18:42:25 -0800

    finished final

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ToastService } from '../toast/toast.service';
 import { Subject } from 'rxjs';
+import { LocalStorageService } from '../localStorageService';
+
+export interface IUser {
+  id?: number;
+  username: string;
+  password: string;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,11 +17,37 @@ import { Subject } from 'rxjs';
 })
 export class LoginComponent implements OnInit {
 
+  user: IUser = { username: '', password: '' };
+  localStorageService: LocalStorageService<IUser>;
+  currentUser: IUser = null;
   constructor(private router: Router, private toastService: ToastService) {
+    this.localStorageService = new LocalStorageService('user');
   }
 
   ngOnInit() {
+    this.currentUser = this.localStorageService.getItemsFromLocalStorage();
+    console.log('this.currentUser.......', this.currentUser);
+    if (this.currentUser != null) {
+      this.router.navigate(['']);
+    }
+  }
 
+  login(user: IUser) {
+    console.log('from login user', user);
+    const defaultUser: IUser = { username: 'akaljot', password: 'akaljot123' };
+    if (user.username !== '' && user.password !== '') {
+      if (user.username === defaultUser.username && user.password === defaultUser.password) {
+
+        this.localStorageService.saveItemsToLocalStorage(user);
+
+        this.router.navigate(['cart', user]);
+      } else {
+        this.toastService.showToast('danger', 1000, 'Login failed. Please check your username or password!');
+      }
+    } else {
+      this.toastService.showToast('danger', 1000, 'Login failed. Please specify username and password!');
+    }
   }
 
 }
+
